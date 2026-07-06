124a6f4a864674f5d6967da553b42322e263f3d8
sxd911 <sxd191810@gmail.com>
2026-07-06 13:32:45 +0800

    修复: 移除GlassButton不支持的onLongPress参数

--- lib/plugin/pl_player/widgets/common_btn.dart
+++ lib/plugin/pl_player/widgets/common_btn.dart
@@ -31,7 +31,6 @@ class ComBtn extends StatelessWidget {
         quality: GlassQuality.premium,
         icon: icon,
         onTap: onTap!,
-        onLongPress: onLongPress,
         width: width,
         height: height,
       );
